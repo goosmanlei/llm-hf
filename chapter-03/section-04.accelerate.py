5ede24db6c53ccef33d5d273ac1e0b0c680d1841
goosmanlei <leiguoguo.ai@gmail.com>
2026-03-07 15:50:18 +0000

    fix bug

--- chapter-03/section-04.accelerate.py
+++ chapter-03/section-04.accelerate.py
@@ -3,6 +3,61 @@ from accelerate import Accelerator
 from torch.optim import AdamW
 from transformers import AutoModelForSequenceClassification, get_scheduler
 
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from transformers.data import data_collator
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = 'bert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+tokenized_datasets = tokenized_datasets.remove_columns(["sentence1", "sentence2", "idx"])
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format('torch')
+
+batch_size = 16
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(
+    tokenized_datasets["train"],
+    shuffle=True,
+    batch_size=batch_size,
+    collate_fn=data_collator,
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["validation"],
+    batch_size=batch_size,
+    collate_fn=data_collator,
+)
+
+from transformers import AutoModelForSequenceClassification
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    checkpoint, num_labels=2
+)
+
+from torch.optim import AdamW
+
+optimizer = AdamW(model.parameters(), lr=5e-5)
+
+from transformers import get_scheduler
+
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
 accelerator = Accelerator()
 
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
@@ -21,6 +76,7 @@ lr_scheduler = get_scheduler(
     num_training_steps=num_training_steps,
 )
 
+from tqdm.auto import tqdm
 progress_bar = tqdm(range(num_training_steps))
 
 model.train()
